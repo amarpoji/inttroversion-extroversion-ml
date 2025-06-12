5cbb27703606790368dbba36683ebc3efd897286
amarpoji <amarpoji1999@gmail.com>
2025-06-12 13:21:48 +0800

    data transformation

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 # main.py
 from src.component.data_ingestion import DataIngestion
+from src.component.data_transformation import DataTransformation
 
 if __name__ == "__main__":
-    ingestion = DataIngestion()
-    train_path, test_path = ingestion.initiate_data_ingestion()
-    print(f"✅ Data Ingestion Completed!")
-    print(f"📁 Train file: {train_path}")
-    print(f"📁 Test file: {test_path}")
+    # ingestion = DataIngestion()
+    # train_path, test_path = ingestion.initiate_data_ingestion()
+    # print(f"✅ Data Ingestion Completed!")
+    # print(f"📁 Train file: {train_path}")
+    # print(f"📁 Test file: {test_path}")
+
+    transformer = DataTransformation()
+    X_train, X_test, y_train, y_test = transformer.initiate_data_transformation(
+    train_path="artifacts/train.csv",
+    test_path="artifacts/test.csv")
+    print(f'data tranformation completed')
+    
+
+
---
+++ src/component/data_transformation.py
@@ -0,0 +1,99 @@
+import os
+import sys
+import pandas as pd
+import numpy as np
+from dataclasses import dataclass
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.impute import SimpleImputer
+from sklearn.model_selection import train_test_split
+import joblib
+
+from src.logger import logging
+from src.exception import CustomException
+from src.utils import save_object
+
+@dataclass
+class DataTransformationConfig:
+    preprocessor_obj_file_path = os.path.join("artifacts", "preprocessor.pkl")
+
+class DataTransformation:
+    def __init__(self):
+        self.config = DataTransformationConfig()
+
+    def get_data_transformer_object(self):
+        '''
+        This function returns the preprocessing pipeline.
+        '''
+        try:
+            numerical_features = [
+                "Time_spent_Alone",
+                "Social_event_attendance",
+                "Going_outside",
+                "Friends_circle_size",
+                "Post_frequency"
+            ]
+
+            categorical_features = ['Stage_fear','Drained_after_socializing']
+
+            numerical_pipeline = Pipeline(steps=[
+                ("imputer", SimpleImputer(strategy="median")),
+                ("scaler", StandardScaler())
+            ])
+
+            categorical_pipeline = Pipeline(steps= [
+                ('imputer', SimpleImputer(strategy= 'most_frequent')),
+                ('one_hot_encoder', OneHotEncoder()),
+                ('scaler',StandardScaler(with_mean= False))
+            ])
+
+            preprocessor = ColumnTransformer([
+                ("num_pipeline", numerical_pipeline, numerical_features),
+                ('cat_pipeline', categorical_pipeline, categorical_features)
+            ])
+
+            logging.info('Numerical columns standard scaler is completed')
+
+            logging.info('Categorical columns encoding is completed')
+
+            logging.info(f'Numerical columns :{numerical_features}')
+            logging.info(f'categorical columns :{categorical_features}')
+
+            return preprocessor
+
+        except Exception as e:
+            raise CustomException(e, sys)
+
+    def initiate_data_transformation(self, train_path, test_path):
+        try:
+            train_df = pd.read_csv(train_path)
+            test_df = pd.read_csv(test_path)
+
+            logging.info("Read train and test data")
+
+            target_column = "Personality"
+
+            input_features_train = train_df.drop(columns=[target_column], axis=1)
+            target_feature_train = train_df[target_column]
+
+            input_features_test = test_df.drop(columns=[target_column], axis=1)
+            target_feature_test = test_df[target_column]
+
+            preprocessor = self.get_data_transformer_object()
+
+            logging.info("Fitting and transforming data...")
+
+            X_train = preprocessor.fit_transform(input_features_train)
+            X_test = preprocessor.transform(input_features_test)
+
+            # Save the preprocessor
+            save_object(
+                file_path=self.config.preprocessor_obj_file_path,
+                obj=preprocessor
+            )
+
+            return X_train, X_test, target_feature_train, target_feature_test
+
+        except Exception as e:
+            raise CustomException(e, sys)
